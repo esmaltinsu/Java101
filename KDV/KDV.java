f51801c0dba8321cb7dcc71cf3737c9cf564d971
Esma Altınsu <esmaltinsu@gmail.com>
2022-10-16 00:42:36 +0300

    Kdv Oranı Hesaplama
    KDV calculator has been added.

--- KDV/KDV.java
+++ KDV/KDV.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class KDV {
     public static void main(String[] args) {
-        System.out.println();
+        /* Eğer girilen tutar 0 ve 1000 TL arasında
+         ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını
+         %8 olarak KDV tutarı hesaplayan programı yazınız. */
+        double tutar , kdvTutar , kdvliTutar , kdvOrani;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ücret Tutarını Giriniz: ");
+        tutar = input.nextDouble();
+
+        kdvOrani= tutar<=1000 ? 0.18 : 0.08;
+
+        kdvTutar = tutar * kdvOrani;
+        kdvliTutar = tutar + kdvTutar;
+
+
+        System.out.println("KDV'siz Tutar : " +tutar);
+        System.out.println("KDV Oranı : " + kdvOrani);
+        System.out.println("KDV Tutarı : " + kdvTutar);
+        System.out.println ("KDV'li Toplam Tutar : " + kdvliTutar);
+
+
     }
 }
